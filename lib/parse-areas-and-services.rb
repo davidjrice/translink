31e3f2e18aa6da4634171fa072c84f5efd92dac1
David Rice <me@davidjrice.co.uk>
2010-01-22 19:50:12 +0000

    Add misc-services data, metro buses that do not fit into the 1..12 range for top level areas. Add parser to extract from original services output from separate data mining script.

--- lib/parse-areas-and-services.rb
+++ lib/parse-areas-and-services.rb
@@ -6,6 +6,7 @@ services_path = File.join(File.dirname(__FILE__), '..', 'public', 'data', 'servi
 areas_path = File.join(File.dirname(__FILE__), '..', 'public', 'data', 'areas.json')
 
 output_path = File.join(File.dirname(__FILE__), '..', 'public', 'data', 'services-new.json')
+misc_output_path = File.join(File.dirname(__FILE__), '..', 'public', 'data', 'misc-services.json')
 
 
 areas = JSON.parse File.read(areas_path)
@@ -21,19 +22,52 @@ puts "SERVICES: #{services.size}"
 
 area_codes = areas.map { |a| a['code'].to_i }
 
+used_services = []
 
 output = {}
 # Iterate each area
 areas.each do |area|
-  serviced = services.select { |s| s['code'] =~ /#{area['code']}\w/ }
-  serviced.delete_if { |s| !s['code'].to_i == area['code'].to_i }
-  codes = serviced.map { |s| s['code'].gsub(area['code'],'') }
-  output[area['code']] = codes
+  serviced = services.select { |s| s['code'] =~ /^(#{area['code']}){1}[a-zA-Z]{1}$/ }
+  #serviced = serviced.delete_if { |s| !s['code'].to_i == area['code'].to_i }
+  
+  area_services = {}
+  seen_codes = []
+  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"].each do |letter|
+    grouped_services = serviced.select { |s| s['code'] =~ /^#{area['code']}{1}#{letter}{1}$/ }
+    
+    if grouped_services.size > 0
+      # Inbound first
+      inbound = grouped_services.select{|s| s['url'] =~ /I.asp$/ }.first
+    
+      # Outbound second
+      outbound = grouped_services.select{|s| s['url'] =~ /O.asp$/ }.first
+    
+      code = inbound['code'].gsub(area['code'],'')
+    
+      area_services[code] = {}
+      if outbound
+        area_services[code]["O"] = outbound['name']
+      end
+      if inbound
+        area_services[code]["I"] = inbound['name']
+      end
+    end
+  end
+  used_services << serviced
+  
+  #codes = serviced.map { |s| s['code'].gsub(area['code'],'') }
+  output[area['code']] = area_services
 end
 
 File.open output_path, "w+" do |file|
   file.flush
   file << JSON.pretty_generate(output)
+end
+
+unused = services - used_services 
+File.open misc_output_path, "w+" do |file|
+  file.flush
+  file << JSON.pretty_generate(unused)
 end
 
 # extract services for that area
