4d2839cea412ed6e38c5805e2097c640e7ae4e77
David Rice <me@davidjrice.co.uk>
2010-01-26 09:19:34 +0000

    Add prototype timetable view to debug data

--- app/controllers/translink_controller.rb
+++ app/controllers/translink_controller.rb
@@ -64,8 +64,16 @@ class TranslinkController < ApplicationController
     @route = Route.find_by_area_id_and_service_id_and_code(area.id, service.id, params[:route])
     @stops = @route.stops.find(:all, :order => "position")
     
-    timetable_path = File.join(RAILS_ROOT, 'public', 'timetables', @route.full_code_hyptenated, '.json')
-    @timetable = JSON.parse(File.read(timetable_path))
+    timetable_path = File.join(RAILS_ROOT, 'public', 'timetables', "#{@route.full_code_hyptenated}.json")
+    times = JSON.parse(File.read(timetable_path))
+    
+    @timetable = {}
+    times.each do |frequency, journeys|
+      @timetable[frequency] = []
+      journeys['journeys'].each do |journey|
+        @timetable[frequency] << journey
+      end
+    end
 
     respond_to do |format|
       format.html { render }
