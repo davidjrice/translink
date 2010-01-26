2f3f61f9f041bf54e0617abd7f7f1667ff473925
David Rice <me@davidjrice.co.uk>
2010-01-26 08:26:37 +0000

    Cleanup order, add todos to issues list

--- script/order.rb
+++ script/order.rb
@@ -1,6 +1,6 @@
 load File.join(File.dirname(__FILE__), '..', 'config', 'environment.rb')
 
-def find_next_nearest(from, stops)
+def nearest_neighbour(from, stops)
   from_coord    = Geodesic::Position.new(from.lat.to_f, from.lon.to_f)
   stops.each do |to|
     to_coord = Geodesic::Position.new(to.lat.to_f, to.lon.to_f)
@@ -13,33 +13,19 @@ areas = Area.find(:all)
 areas.each do |area|
   area.services.each do |serv|
     serv.routes.each do |route|
-      
-      ## NEAREST NEIGHBOUR
       nodes = route.stops.clone
-      $CBD = Stop.new(:lat => "54.596571", :lon => "-5.930235")
-
+      $CBD = Stop.new(:lat => "54.596571", :lon => "-5.930235") # City Hall
       ordered = []
-      node = find_next_nearest($CBD, nodes).first
+      node = nearest_neighbour($CBD, nodes).first
       ordered << node
       nodes.delete(node)
       
       while( !nodes.empty? ) do
-        node = find_next_nearest(node, nodes).first
+        node = nearest_neighbour(node, nodes).first
         ordered << node
         nodes.delete(node)
       end
-      #
-      # TODO
-      #   - extract a full_code method for reuse
-      #
-      # puts "#{area.code}#{serv.code}#{route.code}"
       ordered.each_with_index do |node, i|
-        #
-        # TODO 
-        #   - investigate issue with several Routes
-        #
-        # 2GO, 2GI, 2GX, 2HO, 2HI, 2HX
-        #
         unless node.nil?
           RouteStop.find_by_stop_id_and_route_id(node.id, route.id).update_attribute(:order,i)
         end
